f2042dbab5c15095bed2ee0cf5ec0406f57bfbb2
phartenfeller <pharti@hotmail.de>
2025-09-10 13:03:52 +0200

    updated installation script

--- install_uc_ai.sql
+++ install_uc_ai.sql
@@ -25,22 +25,26 @@ PROMPT - Installing utility functions...
 @@src/dependencies/key_function.sql
 
 PROMPT - Installing API package specifications...
-@@src/packages/uc_ai_message_api.pks
 @@src/packages/uc_ai_tools_api.pks
+@@src/packages/uc_ai_message_api.pks
+@@src/packages/uc_ai_structured_output.pks
 
 PROMPT - Installing AI provider package specifications...
 @@src/packages/uc_ai_anthropic.pks
 @@src/packages/uc_ai_google.pks
+@@src/packages/uc_ai_oci.pks
 @@src/packages/uc_ai_ollama.pks
 @@src/packages/uc_ai_openai.pks
 
 PROMPT Installing package bodies (implementations)...
 PROMPT - Installing API package bodies...
-@@src/packages/uc_ai_message_api.pkb
 @@src/packages/uc_ai_tools_api.pkb
+@@src/packages/uc_ai_message_api.pkb
+@@src/packages/uc_ai_structured_output.pkb
 PROMPT - Installing AI provider package bodies...
 @@src/packages/uc_ai_anthropic.pkb
 @@src/packages/uc_ai_google.pkb
+@@src/packages/uc_ai_oci.pkb
 @@src/packages/uc_ai_ollama.pkb
 @@src/packages/uc_ai_openai.pkb
 PROMPT - Installing core UC AI package body...
